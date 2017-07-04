df0b3717e764a35d893cd03966f7151493aa417e
Vishrut Reddi <vishrutreddi@gmail.com>
2017-07-03 21:03:03 -0700

    Added GetHero Related Data

---
+++ Models/HeroFactory.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const Hero = require('./Hero');
+
+const OpenDotaBaseUri = "https://api.opendota.com";
+
+class HeroFactory {
+
+    constructor() {
+        this.heros = [];
+    }
+
+    getHeros() {
+        return axios.get(OpenDotaBaseUri + '/api/heroStats').then((response) => {
+
+            for(let id = 0; id < response.data.length; ++id) {
+
+                let data = response.data[id],
+                    hero = new Hero();
+
+                hero.name = data.localized_name;
+                hero.primaryAttr = this.getFriendlyHeroAttributeString(data.primary_attr);
+                hero.roles = data.roles;
+                hero.legs = data.legs;
+                hero.image = OpenDotaBaseUri + data.img;
+                hero.icon = OpenDotaBaseUri + data.icon;
+
+                this.heros[data.id] = hero;
+            }
+        });
+    }
+
+    getFriendlyHeroAttributeString(query) {
+        let friendlyStrings = {
+            "agi": "Agility",
+            "melee": "Melee",
+            "int": "Intelligence"
+        }
+
+        return friendlyStrings[query] === undefined ? query : friendlyStrings[query];
+    }
+}
+
+module.exports = HeroFactory;

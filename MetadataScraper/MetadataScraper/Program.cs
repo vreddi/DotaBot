9e5e968e1d0a23b54ca4a39c5ae46ce51312e7ec
Martin Loewenthal <marloe@microsoft.com>
2017-07-24 16:27:00 -0700

    parsed all heroes talents and skills and appended to opendota structure

---
+++ MetadataScraper/MetadataScraper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace MetadataScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var success = AsyncMain().Result;
+        }
+
+        static async Task<bool> AsyncMain()
+        {
+            var heroes = await Scraper.GetAllHeroes();
+            var serializerSettings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            System.IO.File.WriteAllText(@"C:\Repos\DotaBot\Metadata\Heroes.json", JsonConvert.SerializeObject(heroes, serializerSettings));
+
+            return true;
+        }
+    }
+}
